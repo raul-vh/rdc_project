7c3c3c439af20c66eb39ab75208e4c6367c2703b
raul-vh <raulvanharmelen@gmail.com>
2024-01-21 23:06:49 +0100

    Added .env to .gitignore. Updated README.md with more clear instructions.

--- README.md
+++ README.md
@@ -1,11 +1,28 @@
 # rdc_project
 
-First the conda environment should be created in order to have the compatible version of python and required packages. This can be done using the following command:(To be run in the same directory as the environment.yml file.)
-`conda env create -f environment.yml`
+First the conda environment should be created in order to have the compatible version of python and required packages. This can be done using the following command: (To be run in the same directory as the environment.yml file.)
+
+```sh
+conda env create -f environment.yml
+```
+
+Then the conda environment schould be activated in the terminal before working on the project:
+[
+
+```sh
+conda activate rdc_project
+```
+
+[Link to official conda documentation for more info:]([Managing environments &#8212; conda 23.11.1.dev56 documentation](https://docs.conda.io/projects/conda/en/latest/user-guide/tasks/manage-environments.html))
 
 This project should by run with a .env file in the directory, containing the url of the google sheet:
 
-``` yml
+```yml
 GOOGLESHEETS_URL='<my_super_secret_url>'
+```
 
+The streamlit server can be started locally with:
+
+```python
+streamlit run app.py
 ```
--- app.py
+++ app.py
@@ -8,12 +8,24 @@ import plotly.express as px
 load_dotenv()
 url = os.getenv('GOOGLESHEETS_URL')
 
-try:
-    df = duckdb.sql(
-        f"SELECT * FROM read_csv_auto('{url}')").df()
-except Exception as exeption:
-    print(str(exeption))
+def connect_to_google_docs():
+    """
+    Connect to google docs sheet with survey results using duckdb.
+    Returns dataframe.
+    """
+    try:
+        df = duckdb.sql(
+            f"SELECT * FROM read_csv_auto('{url}')").df()
+    except Exception as exeption:
+        print(str(exeption))
+    return df
 
-st.set_page_config(page_title="Onderzoek wachtwoordgebruik en digitale veiligheid", page_icon=":bar_chart:", layout="wide")
-
-st.write(df)
+if __name__ == "__main__":
+    df = connect_to_google_docs()
+    st.set_page_config(page_title="Onderzoek wachtwoordgebruik en digitale veiligheid", page_icon=":bar_chart:", layout="wide")
+    st.write(df)
+    st.header("Structuur van dit digitaal instrument.")
+    st.write("**Inleiding:** Empirische beschrijving van het voorbeeld.")
+    st.write("**De bedreigingen:** Welke bedreigingen zijn er? Hoe goed zijn mensen nu beveiligd tegen deze?")
+    st.write("**De oplossingen:** Welke oplossingen zijn er? Waarom zijn ze nodig? *Hier bespreken we wat de effecten zijn op de veiligheid*")
+    st.write("**Welke oplossing past bij mij?** *Hier gaan we proberen te verklaren waarom mensen bepaald gedrag vertonen. De schaal van veiligheid vs gebruiksgemak.*")
